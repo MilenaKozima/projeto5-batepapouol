248c9b6d4b78dcc3fe461a322db094b6345cd923
MilenaKozima <mkozima06@gmail.com>
2023-04-16 22:00:22 -0300

    Versão enviando mensage feito

--- script.js
+++ script.js
@@ -1,2 +1,41 @@
+const Nome = prompt("Qual o seu nome ?");
 
+const Mensagens = [
 
+];
+
+axios.defaults.headers.common['Authorization'] = 'xLBzuiWi6qYVmyUXY7S8o3l7';
+
+function aparecerMensagens(){
+    const ulmensagens = document.querySelector('.Mensagens');
+    ulmensagens.innerHTML = '';
+
+    for(let i = 0; i<Mensagens.length; i++){
+        let menssagem = Mensagens[i];
+        ulmensagens.innerHTML += `            
+        <li>
+            <div class="aparecer">
+                <p>${menssagem.from} para ${menssagem.to}: ${menssagem.text}</p> 
+            </div>
+        </li>`
+        ;
+    }
+}
+
+//aparecerMensagens();
+
+function adicionarMensagem(){
+    const inputMensagem = document.querySelector('input');
+    
+    const novaMensagem = {
+        from: Nome,
+        to: "Todos",
+        text: inputMensagem.value,
+        type: "message" // ou "private_message" para o bônus
+    };
+    
+    Mensagens.push(novaMensagem);
+
+    aparecerMensagens();
+
+}
